85eb3ba4cd35697be8649633f38d490a71720b99
kajalsah7611 <kajalsah7611@gmail.com>
2024-02-08 23:22:19 +0530

    add delete functionality

--- src/components/EntryGrid.js
+++ src/components/EntryGrid.js
@@ -1,14 +1,23 @@
 // EntryGrid.js
 import React from 'react';
 
-const EntryGrid = ({ entries }) => {
+const EntryGrid = ({ entries, setEntries,index}) => {
+
+  const deleteTask = (id) => {
+    // Filter out the task with the given ID
+    const updatedEntries = entries.filter(entry => index!== id);
+    // Update the tasks state with the filtered tasks
+    setEntries(updatedEntries);
+  };
+
   return (
 <>
     <h2>Entries</h2>
     <div className="grid">
      
-      {entries.map((entry, index) => (
+      {entries.map((entry) => (
         <div key={index} className="entry">
+          <button onClick={() => deleteTask(index)}>Delete</button>
           <p>Name: {entry.name}</p>
           <p>Address: {entry.address}</p>
           <p>Mobile Number: {entry.mobileNumber}</p>
