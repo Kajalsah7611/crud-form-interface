04e01011be464ad1e66aecbc8653635760ef1e4f
kajalsah7611 <kajalsah7611@gmail.com>
2024-02-12 23:20:35 +0530

    add component EntryGrid

--- src/components/EntryGrid.js
+++ src/components/EntryGrid.js
@@ -1,32 +1,24 @@
 // EntryGrid.js
 import React from 'react';
+import EachEntry from './EachEntry';
 
-const EntryGrid = ({ entries, setEntries,index}) => {
+const EntryGrid = ({ entries, onDelete}) => {
 
-  const deleteTask = (id) => {
-    // Filter out the task with the given ID
-    const updatedEntries = entries.filter(entry => index!== id);
-    // Update the tasks state with the filtered tasks
-    setEntries(updatedEntries);
-  };
+
 
   return (
 <>
     <h2>Entries</h2>
     <div className="grid">
      
-      {entries.map((entry) => (
-        <div key={index} className="entry">
-          <button onClick={() => deleteTask(index)}>Delete</button>
-          <p>Name: {entry.name}</p>
-          <p>Address: {entry.address}</p>
-          <p>Mobile Number: {entry.mobileNumber}</p>
-          <p>Email: {entry.email}</p>
-          <p>Message: {entry.message}</p>
-        </div>
+      {entries.map((entry, index) => (
+        <EachEntry key={entry.id} entry={entry} onDelete={() => onDelete(index) }/>
+      
       ))}
-    </div></>
+    </div>  </>
   );
+
+  
 };
 
 export default EntryGrid;
